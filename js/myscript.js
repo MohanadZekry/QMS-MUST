cc1b13d843b4cfe17a67b64124611639106b8725
MohanadZekry <50669603+MohanadZekry@users.noreply.github.com>
2020-07-28 10:28:33 +0200

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,14 @@
-
-			function myFunction() {
-				  sessionStorage.setItem("department", "");
-                  sessionStorage.setItem("category", "");
-                  sessionStorage.setItem("name", "");
-                  sessionStorage.setItem("mail", "");
-                  console.log("hello");
+function myFunction() {
+sessionStorage.setItem("department", "");
+sessionStorage.setItem("category", "");
+sessionStorage.setItem("name", "");
+sessionStorage.setItem("mail", "");
             }
-      function enter(){
-            var input = document.getElementById("submit");
-            input.addEventListener("keyup", function(event) {
-              if (event.keyCode === 13) {
-               event.preventDefault();
-               document.getElementById("submit").click();
+function enter(){
+var input = document.getElementById("submit");
+input.addEventListener("keyup", function(event) {
+if (event.keyCode === 13) {
+event.preventDefault();
+document.getElementById("submit").click();
               }
             });}
