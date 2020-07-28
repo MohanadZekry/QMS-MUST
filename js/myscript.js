db2366be721be6df2813efc1ed3ebf6b88549965
Mahmoud Mokhtar <50672478+mahmoudmokhtarovic@users.noreply.github.com>
2020-07-28 10:17:52 +0200

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+
+			function myFunction() {
+				  sessionStorage.setItem("department", "");
+                  sessionStorage.setItem("category", "");
+                  sessionStorage.setItem("name", "");
+                  sessionStorage.setItem("mail", "");
+                  console.log("hello");
+            }
+      function enter(){
+            var input = document.getElementById("submit");
+            input.addEventListener("keyup", function(event) {
+              if (event.keyCode === 13) {
+               event.preventDefault();
+               document.getElementById("submit").click();
+              }
+            });}
